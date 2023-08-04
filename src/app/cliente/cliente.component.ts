c56b120a032d78f2bd807b2de9eb2c4778236329
IamThiago-IT <thiagodossantos315@hotmail.com>
2023-08-04 15:51:03 -0300

    add pedido, função aprovar e rejeitar pedido

--- src/app/cliente/cliente.component.ts
+++ src/app/cliente/cliente.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-cliente',
@@ -9,6 +10,14 @@ export class ClienteComponent {
   pedidosEmAberto: any[] = [
     { numeroPedido: 1, descricao: 'Pedido 1' },
     { numeroPedido: 2, descricao: 'Pedido 2' },
+
     // Add more sample data here or fetch data from the backend.
   ];
+
+  constructor(private router: Router) {}
+
+  irParaPedido() {
+    // Navega para a página de pedido
+    this.router.navigate(['/pedido']);
+  }
 }
